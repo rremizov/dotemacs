93a17147248f84e4e6f57e1bce339d8d8bef2ea7
ceaseandrepent <ceaseandrepent@gmail.com>
2013-08-04 12:34:54 +0600

    mode-hooks.el: spaces for python code indentation.

--- init.d/mode-hooks.el
+++ init.d/mode-hooks.el
@@ -4,11 +4,9 @@
 		(setq indent-tabs-mode t)
 		(when (y-or-n-p "Auto Fill mode? ")
 			(set-fill-column 60)
-			(turn-on-auto-fill))
-))
+			(turn-on-auto-fill))))
 
 (add-hook 'python-mode-hook
 	(lambda()
 		(setq tab-width 4)
-		(setq indent-tabs-mode t)
-))
+		(setq indent-tabs-mode nil)))
