66e0209399ef6b19d4f3dfa3fcf0719f7eebd2a7
rremizov <rremizov@yandex.ru>
2013-12-08 15:52:38 +0600

    fix indentation

--- init.d/autosaves-and-backups.el
+++ init.d/autosaves-and-backups.el
@@ -5,23 +5,23 @@
       delete-old-versions t
       version-control t)
 
-;; Save all tempfiles in $TMPDIR/emacs$UID/                                                        
+;; Save all tempfiles in $TMPDIR/emacs$UID/
 (defconst emacs-tmp-dir (format "%s/%s%s/" temporary-file-directory "emacs" (user-uid)))
 (setq backup-directory-alist
-	`((".*" . ,emacs-tmp-dir)))
+      `((".*" . ,emacs-tmp-dir)))
 (setq auto-save-file-name-transforms
-	`((".*" ,emacs-tmp-dir t)))
+      `((".*" ,emacs-tmp-dir t)))
 (setq auto-save-list-file-prefix
-	  emacs-tmp-dir)
+      emacs-tmp-dir)
 
 ;; Delete files not accessed in a week
 (message "Deleting old backup files...")
 (let ((week (* 60 60 24 7))
-		(current (float-time (current-time))))
-	(dolist (file (directory-files temporary-file-directory t))
-		(when (and (backup-file-name-p file)
-				(> (- current (float-time (fifth (file-attributes file))))
-				week))
-			(message "%s" file)
-			(delete-file file))))
+      (current (float-time (current-time))))
+  (dolist (file (directory-files temporary-file-directory t))
+    (when (and (backup-file-name-p file)
+               (> (- current (float-time (fifth (file-attributes file))))
+                  week))
+      (message "%s" file)
+      (delete-file file))))
 
--- init.d/mode-hooks.el
+++ init.d/mode-hooks.el
@@ -1,15 +1,15 @@
 (add-hook 'text-mode-hook
-	(lambda()
-		(setq tab-width 4)
-		(setq indent-tabs-mode t)
-		(when (y-or-n-p "Auto Fill mode? ")
-			(set-fill-column 60)
-			(turn-on-auto-fill))))
+          (lambda()
+            (setq tab-width 4)
+            (setq indent-tabs-mode t)
+            (when (y-or-n-p "Auto Fill mode? ")
+              (set-fill-column 60)
+              (turn-on-auto-fill))))
 
 (add-hook 'python-mode-hook
-	(lambda()
-		(setq tab-width 4)
-		(setq indent-tabs-mode nil)))
+          (lambda()
+            (setq tab-width 4)
+            (setq indent-tabs-mode nil)))
 
 (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
 (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
@@ -19,9 +19,9 @@
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
 (add-hook 'lisp-mode-hook
-	(lambda()
-		(set (make-local-variable lisp-indent-function)
-			'common-lisp-indent-function)
-		(setq tab-width 4)
-		(setq indent-tabs-mode nil)))
+          (lambda()
+            (set (make-local-variable lisp-indent-function)
+                 'common-lisp-indent-function)
+            (setq tab-width 4)
+            (setq indent-tabs-mode nil)))
 
