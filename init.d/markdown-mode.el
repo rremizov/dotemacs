62400cb2f8847b5ebdbf8d3b38a3932baad09ca0
rremizov <rremizov@yandex.ru>
2014-01-28 01:36:22 +0600

    new submodule: markdown mode

---
+++ init.d/markdown-mode.el
@@ -0,0 +1,6 @@
+(add-to-list 'load-path "~/.emacs.d/bundle/markdown-mode/")
+(autoload 'markdown-mode "markdown-mode"
+		  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
