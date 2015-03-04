95e6630a352e7a8119fcf2a5983c3ba723197de8
rremizov <rremizov@yandex.ru>
2015-03-04 05:13:51 +0500

    cursor states in evil's modes

--- init.d/evil.el
+++ init.d/evil.el
@@ -1,4 +1,10 @@
-
 (require 'evil)
 (evil-mode 1)
+
+;; Change cursor color depending on mode
+(setq evil-normal-state-cursor '("#8faf9f" box))
+(setq evil-visual-state-cursor '("#53665D" box))
+(setq evil-insert-state-cursor '("#8faf9f" bar))
+(setq evil-replace-state-cursor '("#BA7D77" box))
+(setq evil-operator-state-cursor '("#8faf9f" hollow))
 
