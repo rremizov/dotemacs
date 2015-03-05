4ec2b811fd588faec3fc47f145ed30161e13458e
rremizov <rremizov@yandex.ru>
2015-03-05 05:14:05 +0500

    custom variables for tabs

--- init.d/custom.el
+++ init.d/custom.el
@@ -7,6 +7,8 @@
  '(custom-safe-themes
    (quote
     ("c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
+ '(elscreen-tab-display-control nil)
+ '(elscreen-tab-display-kill-screen nil)
  '(menu-bar-mode nil)
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
@@ -15,4 +17,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(elscreen-tab-background-face ((t (:background "#3f3f3f"))))
+ '(elscreen-tab-current-screen-face ((t (:background "#afd787" :foreground "black"))))
+ '(elscreen-tab-other-screen-face ((t (:background "#3a3a3a" :foreground "Gray50")))))
