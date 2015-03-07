26de0abf281a76556ffff0b61b32198f5f2f63d2
rremizov <rremizov@yandex.ru>
2015-03-07 22:44:37 +0500

    helm hotkeys

--- init.d/hotkeys.el
+++ init.d/hotkeys.el
@@ -7,8 +7,8 @@
 
 ;; Helm
 (define-key evil-normal-state-map " " 'helm-mini)
+(define-key evil-normal-state-map (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
-; (define-key evil-normal-state-map "M-x" 'helm-M-x)
-; (global-set-key (kbd "M-x") 'helm-M-x)
 
