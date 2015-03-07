eb4939cee1e1c8dd64b07fbb10cc8d8c5552795c
rremizov <rremizov@yandex.ru>
2015-03-07 21:41:39 +0500

    helm config

--- init.d/helm.el
+++ init.d/helm.el
@@ -1,5 +1,10 @@
 (require 'helm-config)
 (helm-mode 1)
 (helm-autoresize-mode 1)
-(setq helm-buffers-fuzzy-matching t)
+
+(setq helm-buffers-fuzzy-matching t
+	  helm-M-x-fuzzy-match t
+	  helm-semantic-fuzzy-match t
+	  helm-imenu-fuzzy-match t
+	  helm-recentf-fuzzy-match t)
 
