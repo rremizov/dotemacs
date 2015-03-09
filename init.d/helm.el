a00bf2e2cd615da14250b7a03ca86565d54a26ac
rremizov <rremizov@yandex.ru>
2015-03-09 17:32:52 +0500

    tabs

--- init.d/helm.el
+++ init.d/helm.el
@@ -3,10 +3,10 @@
 (helm-autoresize-mode 1)
 
 (setq helm-buffers-fuzzy-matching t
-	  helm-M-x-fuzzy-match t
-	  helm-semantic-fuzzy-match t
-	  helm-imenu-fuzzy-match t
-	  helm-recentf-fuzzy-match t)
+      helm-M-x-fuzzy-match t
+      helm-semantic-fuzzy-match t
+      helm-imenu-fuzzy-match t
+      helm-recentf-fuzzy-match t)
 
 (require 'helm-descbinds)
 (helm-descbinds-mode)
