8149610263c347f4d9c57d7e2dc76ae87e90cde5
rremizov <rremizov@yandex.ru>
2015-03-12 03:02:50 +0500

    use jk to exit insert mode

--- init.d/hotkeys.el
+++ init.d/hotkeys.el
@@ -12,6 +12,28 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 
+
 ;; Projectile
 (define-key evil-normal-state-map (kbd "C-p") 'helm-projectile)
+
+
+;; Evil
+(global-set-key [escape] 'evil-exit-emacs-state)
+
+(define-key evil-insert-state-map "j" #'cofi/maybe-exit)
+(evil-define-command cofi/maybe-exit ()
+  :repeat change
+  (interactive)
+  (let ((modified (buffer-modified-p)))
+	(insert "j")
+	(let ((evt (read-event (format "Insert %c to exit insert state" ?k)
+               nil 0.5)))
+      (cond
+       ((null evt) (message ""))
+       ((and (integerp evt) (char-equal evt ?k))
+    (delete-char -1)
+    (set-buffer-modified-p modified)
+    (push 'escape unread-command-events))
+       (t (setq unread-command-events (append unread-command-events
+                          (list evt))))))))
 
