55c1b1a573107f43c7aa37d86224f573599dda9a
rremizov <rremizov@yandex.ru>
2015-04-03 04:40:57 +0500

    set PATH, VIRTUAL_ENV, exec-path

--- environment.el
+++ environment.el
@@ -1,11 +1,25 @@
-; (flycheck-set-checker-executable
-;  'python-pylint
-;  (concat (getenv "VIRTUAL_ENV") "/bin/pylint"))
+;;; package --- Summary
 
-;; (when (not (= (getenv "VIRTUAL_ENV"))))
+
+;;; Commentary:
+
+
+;;; Code:
 
 ; PATH=$(pwd)/pyenv/bin:$PATH
 ; VIRTUAL_ENV=$(pwd)/pyenv/
 ; PYTHONPATH=$(pwd)/server/src/:$PYTHONPATH
 ; emacs
+
+(when (directory-files "." nil "pyenv")
+  (message "[ENVIRONMENT] Detected python virtualenv")
+
+  (add-to-list 'exec-path (concat (expand-file-name ".") "/pyenv/bin/"))
+  (setenv "PATH" (concat (expand-file-name ".") "/pyenv/bin/:" (getenv "PATH")))
+  (message (concat "[ENVIRONMENT] [PATH] " (getenv "PATH")))
+
+  (setenv "VIRTUAL_ENV" (concat (expand-file-name ".") "/pyenv/"))
+  (message (concat "[ENVIRONMENT] [VIRTUAL_ENV] " (getenv "VIRTUAL_ENV"))))
+
+;;; environment.el ends here
 
