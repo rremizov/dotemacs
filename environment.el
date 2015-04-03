73e71ab29dafac8d2772b9355afa812e3924fe60
rremizov <rremizov@yandex.ru>
2015-04-03 05:55:15 +0500

    detect django project and add it to PYTHONPATH

--- environment.el
+++ environment.el
@@ -6,10 +6,25 @@
 
 ;;; Code:
 
-; PATH=$(pwd)/pyenv/bin:$PATH
-; VIRTUAL_ENV=$(pwd)/pyenv/
-; PYTHONPATH=$(pwd)/server/src/:$PYTHONPATH
-; emacs
+(defun update-pythonpath (filename path)
+  "Look for the FILENAME inside PATH.  Add folder with FILENAME to pythonpath."
+
+  (let ((path-to-filename
+         (locate-file filename
+                      (mapcar (lambda (item) (concat (expand-file-name ".") item))
+                              path))))
+    (when path-to-filename
+      (setenv "PYTHONPATH"
+              (concat (replace-regexp-in-string (concat filename "$")
+                                                ""
+                                                path-to-filename)
+                      ":"
+                      (getenv "PYTHONPATH")))
+
+      (message (concat "[ENVIRONMENT] [PYTHONPATH] " (getenv "PYTHONPATH"))))))
+
+(update-pythonpath "manage.py" '("" "/server/src"))
+
 
 (when (directory-files "." nil "pyenv")
   (message "[ENVIRONMENT] Detected python virtualenv")
