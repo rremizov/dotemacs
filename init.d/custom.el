c9413b59568d1fdd07e743a77b3f8a288888822d
rremizov <rremizov@yandex.ru>
2015-04-29 20:31:23 +0500

    desktop save mode

--- init.d/custom.el
+++ init.d/custom.el
@@ -10,6 +10,10 @@
    (quote
     ("c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default)))
  '(delete-trailing-lines nil)
+ '(desktop-auto-save-timeout 3)
+ '(desktop-path (quote ("~/.emacs.d/" "~" ".")))
+ '(desktop-restore-frames nil)
+ '(desktop-save-mode t)
  '(elscreen-tab-display-control nil)
  '(elscreen-tab-display-kill-screen nil)
  '(eshell-directory-name "~/.emacs.d/local/eshell/")
