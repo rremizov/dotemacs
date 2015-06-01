327c8f4c703e5bde463258d4b93980309354c3c1
rremizov <rremizov@yandex.ru>
2015-06-01 20:10:53 +0500

    settings for the projectile-find-other-file

--- init.d/projectile.el
+++ init.d/projectile.el
@@ -1,7 +1,22 @@
+;;; package --- Summary
+
+
+;;; Commentary:
+
+
+;;; Code:
+
 (require 'projectile)
 (require 'helm-projectile)
 (projectile-global-mode)
 
 ;; Helm keeps changing the function's name
 (defalias 'helm-buffers-list--match-fn 'helm-buffers-match-function)
+
+(add-to-list 'projectile-other-file-alist '("js" "jsx" "less" "css"))
+(add-to-list 'projectile-other-file-alist '("jsx" "js" "less" "css"))
+(add-to-list 'projectile-other-file-alist '("less" "js" "jsx" "css"))
+(add-to-list 'projectile-other-file-alist '("css" "js" "jsx" "less"))
+
+;;; projectile.el ends here
 
