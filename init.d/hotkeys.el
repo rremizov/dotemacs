e0c4bd9c1662e7ea511affbdf675d2e49eee5ea0
rremizov <rremizov@yandex.ru>
2015-06-04 04:22:06 +0500

    use C-x b to open helm mini

--- init.d/hotkeys.el
+++ init.d/hotkeys.el
@@ -17,6 +17,7 @@
 (define-key evil-normal-state-map " " 'helm-mini)
 (define-key evil-normal-state-map (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 
