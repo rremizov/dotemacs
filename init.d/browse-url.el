bbf9bd55f0b3b7eea219f99981f2f9cd1f98ceb3
rremizov <rremizov@yandex.ru>
2016-06-05 02:59:29 +0500

    turn off browse-url.el

--- init.d/browse-url.el
+++ init.d/browse-url.el
@@ -1,3 +1,3 @@
-(setq browse-url-browser-function 'browse-url-generic
-	  browse-url-generic-program "google-chrome")
+;; (setq browse-url-browser-function 'browse-url-generic
+;; 	  browse-url-generic-program "google-chrome")
 
