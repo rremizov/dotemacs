188425a77f0dcd292787b4a166bf0aff1d1351ec
rremizov <rremizov@yandex.ru>
2018-02-06 20:15:14 +0500

    flycheck-mypy

--- init.d/flycheck.el
+++ init.d/flycheck.el
@@ -11,5 +11,6 @@
 ;(require 'go-flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (flycheck-add-next-checker 'python-flake8 'python-pylint)
+(flycheck-add-next-checker 'python-pylint 'python-mypy)
 
 ;;; flycheck.el ends here
