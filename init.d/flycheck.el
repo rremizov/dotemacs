95b66a687680a5998802a35c0d44b38b73cafb3b
rremizov <rremizov@yandex.ru>
2018-04-13 22:54:28 +0500

    fix python-mypy checker

--- init.d/flycheck.el
+++ init.d/flycheck.el
@@ -12,7 +12,7 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (flycheck-add-next-checker 'python-flake8 'python-pylint)
 
-(when (= 3 (shell-command "python -c 'import sys; sys.stdout.write(str(sys.version_info[0]))'"))
+(when (string= "3" (shell-command-to-string "python -c 'import sys; sys.stdout.write(str(sys.version_info[0]))'"))
   (flycheck-add-next-checker 'python-pylint 'python-mypy))
 
 ;;; flycheck.el ends here
